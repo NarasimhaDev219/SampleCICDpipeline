a37d94f081fe47e4fdb43493fa23512f1c1f3172
narasimharaoy <narasimharaoy.in@mouritech.com>
2023-05-08 14:47:02 +0530

    Add Project to App center

--- app/src/main/java/com/example/samplecicdpipeline/MainActivity.kt
+++ app/src/main/java/com/example/samplecicdpipeline/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.samplecicdpipeline
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "{\"5ae95fd3-1136-47ad-9309-a7fde6708882\"}",
+            Analytics::class.java, Crashes::class.java
+        )
+
     }
 }
